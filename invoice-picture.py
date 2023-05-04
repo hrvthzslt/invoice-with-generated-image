88f36a03ccd8bc93f0d6bdb41269a5df3f8778b3
Zsolt Horváth <hrvth.7slt@gmail.com>
2023-05-04 19:25:37 +0200

    invoice with image

--- invoice-picture.py
+++ invoice-picture.py
@@ -2,6 +2,11 @@ from decouple import config
 from lib.image_generator import ImageGenrator
 from lib.invoice_generator import InvoiceGenerator
 
-#image_url = ImageGenrator(key=config('OPENAI_API_KEY')).generate("cool coding people with computers")
-# print(image_url)
-InvoiceGenerator.generate()
+products = ['2 pizza', '1 amiga 500']
+print(' '.join(products))
+InvoiceGenerator.generate(
+    products,
+    ImageGenrator(key=config('OPENAI_API_KEY')).generate(' '.join(products))
+)
+
+print('Invoice created')
--- lib/invoice_generator.py
+++ lib/invoice_generator.py
@@ -1,28 +1,35 @@
 from pathlib import Path
-from typing import BinaryIO
 
-import borb.pdf
-from borb.pdf import Document, Page, SingleColumnLayout, Paragraph, PDF
+from borb.io.read.types import Decimal
+from borb.pdf import Document, Page, SingleColumnLayout, Paragraph, PDF, Image
 
 
 class InvoiceGenerator:
     @staticmethod
-    def generate():
+    def generate(products: list, image_url: str):
         # empty document
         pdf = Document()
-        
+
         # add page
         page = Page()
         pdf.add_page(page)
-        
+
         # use layout
         layout = SingleColumnLayout(page)
-        
-        # add paragraph
-        layout.add(Paragraph("Hello Hallo"))
-        
+
+        # add image
+        layout.add(
+            Image(
+                image_url,
+                width=Decimal(256),
+                height=Decimal(256),
+            ))
+
+        # add paragraphs
+        layout.add(Paragraph("Purchased products:"))
+        for product in products:
+            layout.add(Paragraph(product))
+
         # store
         with open(Path("output.pdf"), "wb") as pdf_file_handle:
             PDF.dumps(pdf_file_handle, pdf)
-        
-        
